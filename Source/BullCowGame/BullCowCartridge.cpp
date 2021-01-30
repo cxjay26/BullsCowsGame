a313f900bca89d882006ae7bc16b995de5cde1ea
cxjay26 <christianv042393@gmail.com>
2021-01-30 13:09:20 -0500

    Added logic for game

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,11 +4,19 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
-    PrintLine("Welcome to the Bull Cows Game!");
-    PrintLine("Press ENTER to continue...");
+    PrintLine(TEXT("Welcome to the Bull Cows Game!"));
+    PrintLine(TEXT("Guess the 4 letter word!"));// Magic Number Remove!
+    PrintLine(TEXT("Press ENTER to continue..."));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
+    FString HiddenWord = TEXT("cake");//Move outisde this function.
+    if (HiddenWord == Input){
+        PrintLine(TEXT("You win!"));
+    }
+    else {
+        PrintLine(TEXT("Try again..."));
+    }
 }
